634d4e43afbfe4ff046baaee91f488e65aa00f76
Edward <fantom-exe@tuta.io>
2024-07-04 04:21:43 -0700

    adjust alsa profile directories and restart all pipewire services

--- install.sh
+++ install.sh
@@ -26,20 +26,16 @@ fi
 echo "Installing udev hwdb"
 sudo cp etc/udev/hwdb.d/sennheiser-gsx.hwdb /etc/udev/hwdb.d/
 
-echo "Installing pulsaudio profiles"
-read -p "Should we install the channelswap-fix, see https://github.com/evilphish/sennheiser-gsx-1000/issues/9 (y for yes, n [default])? " -n 1 -r
+echo "Installing pipewire profiles"
+read -p "Install the channelswap-fix, see https://github.com/evilphish/sennheiser-gsx-1000/issues/9 (y for yes, n [default])? " -n 1 -r
 echo 
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  sudo cp -r usr/share/pulseaudio/alsa-mixer/profile-sets/sennheiser-gsx-$type-channelswap.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/sennheiser-gsx-$type.conf 
+  sudo cp -r usr/share/alsa-card-profile/mixer/profile-sets/sennheiser-gsx-$type-channelswap.conf /usr/share/alsa-card-profile/mixer/profile-sets/
   echo "- installed channel-swap mix"
 else
-  sudo cp -r usr/share/pulseaudio/alsa-mixer/profile-sets/sennheiser-gsx-$type.conf /usr/share/pulseaudio/alsa-mixer/profile-sets/
+  sudo cp -r usr/share/alsa-card-profile/mixer/profile-sets/sennheiser-gsx-$type.conf /usr/share/alsa-card-profile/mixer/profile-sets/
   echo "- installed normal channel mix"
-fi
-
-if [ -d /usr/share/alsa-card-profile/mixer/profile-sets ]; then
-  sudo ln -s /usr/share/pulseaudio/alsa-mixer/profile-sets/sennheiser-gsx-$type.conf /usr/share/alsa-card-profile/mixer/profile-sets/
 fi
 
 echo "Reloading udev rules"
@@ -47,20 +43,16 @@ sudo systemd-hwdb update
 sudo udevadm control -R
 sudo udevadm trigger
 
-read -p "SKIP pulseaudio be restart () (y for yes, n [default])? " -n 1 -r
+read -p "Restart all Pipewire services now (y for yes, n [default])? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  echo "Skipped pulseaudio restart"
-else
-  echo "Restarting pulse audio"
-  # ignore errors if we restart too often / to fast .. we just ensure to nuke it
-  pulseaudio -k > /dev/null 2>&1 || true
-  pulseaudio -k > /dev/null 2>&1 || true
-  pulseaudio -k > /dev/null 2>&1 || true
-
-  echo "Ensure pulseaudio is started"
+  echo "Restarting wireplumber, pipewire, and pipewire-pulse ..."
+  systemctl --user restart wireplumber pipewire pipewire-pulse
   sleep 2
-  pulseaudio -D
+  echo "Pipewire sould be started now."
+else
+  echo "Skipped restarting. You can do this yourself by running:"
+  echo "systemctl --user restart wireplumber pipewire pipewire-pulse"
 fi
 
